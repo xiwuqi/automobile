625efc6d47e13be03b61c34a8368c72fbfbde31a
xiwuqi <qiyuewuxi@yeah.net>
2023-04-22 22:59:23 +0800

    新建Service层，创建UserService和相应接口函数

---
+++ ads/src/main/java/com/wuxi/ads/service/impl/UserServiceImpl.java
@@ -0,0 +1,110 @@
+package com.wuxi.ads.service.impl;
+
+import com.wuxi.ads.bean.User;
+import com.wuxi.ads.mapper.UserMapper;
+import com.wuxi.ads.service.UserService;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+public class UserServiceImpl implements UserService {
+    @Resource
+    private UserMapper userMapper;
+
+    @Resource
+    private RedisTemplate<Object, Object> redisTemplate;
+
+    @Override
+    public User login(User user) {
+        return userMapper.selectByUsernameAndPasswordAndIsAdmin(user.getUsername(), user.getUserpassword(), user.getIsadmin());
+    }
+
+    @Override
+    public void saveUser(String token, User user) {
+        // 设置redisTemplate对象key的序列化方式
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        // key是token，value是用户保存到redis中，超时时间1小时
+        redisTemplate.opsForValue().set(token, user, 1, TimeUnit.HOURS);
+    }
+
+    @Override
+    public User getUser(String token) {
+        // 根据token得到user
+        return (User) redisTemplate.opsForValue().get(token);
+    }
+
+    @Override
+    public void removeUser(String token) {
+        // 移除token
+        redisTemplate.delete(token);
+    }
+
+    @Override
+    public Integer register(String username, String password) {
+        User tmp = userMapper.selectByUsername(username);
+        if(tmp != null) return 0;  //账号重复
+
+        User user = new User();
+        user.setUsername(username);
+        user.setUserpassword(password);
+        user.setIsadmin((byte)0);
+        return userMapper.insertSelective(user);
+    }
+
+    @Override
+    public void setPassword(Integer id, String password) {
+        User user = new User();
+        user.setUserid(id);
+        user.setUserpassword(password);
+        userMapper.updateByPrimaryKeySelective(user);
+    }
+
+    @Override
+    public Integer getCount() {
+        return userMapper.selectCount();
+    }
+
+    @Override
+    public List<User> queryUsers() {
+        return userMapper.selectAll();
+    }
+
+    @Override
+    public int getSearchCount(Map<String, Object> params) {
+        return userMapper.selectCountBySearch(params);
+    }
+
+    @Override
+    public List<User> searchUsersByPage(Map<String, Object> params) {
+        return userMapper.selectBySearch(params);
+    }
+
+    @Override
+    public Integer addUser(User user) {
+        return userMapper.insertSelective(user);
+    }
+
+    @Override
+    public Integer deleteUser(User user) {
+        if(user.getUserid() == 1) return 0;
+        return userMapper.deleteByPrimaryKey(user.getUserid());
+    }
+
+    @Override
+    public Integer deleteUsers(List<User> users) {
+        int count = 0;
+        for(User user : users) {
+            count += deleteUser(user);
+        }
+        return count;
+    }
+
+    @Override
+    public Integer updateUser(User user) {
+        return userMapper.updateByPrimaryKeySelective(user);
+    }
+}
