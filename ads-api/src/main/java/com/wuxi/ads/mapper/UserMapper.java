77cbf675bc4e27d5fc52e9fcc6753eb9a2344dbb
xiwuqi <qiyuewuxi@yeah.net>
2023-04-24 12:41:16 +0800

    修改相关代码，并添加dao层注解@Repository

--- ads-api/src/main/java/com/wuxi/ads/mapper/UserMapper.java
+++ ads-api/src/main/java/com/wuxi/ads/mapper/UserMapper.java
@@ -1,15 +1,14 @@
 package com.wuxi.ads.mapper;
 
 import com.wuxi.ads.bean.User;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Map;
 
-
-@Repository
-public interface  UserMapper {
+@Mapper
+public interface UserMapper {
     int deleteByPrimaryKey(Integer userid);
 
     int insert(User record);
