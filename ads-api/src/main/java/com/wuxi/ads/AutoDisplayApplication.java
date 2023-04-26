0569564398f037991b17f9c4902e5dd4223e2e64
xiwuqi <qiyuewuxi@yeah.net>
2023-04-26 22:42:44 +0800

    增添事务管理注释和Mapper映射路径注释

--- ads-api/src/main/java/com/wuxi/ads/AutoDisplayApplication.java
+++ ads-api/src/main/java/com/wuxi/ads/AutoDisplayApplication.java
@@ -1,9 +1,13 @@
 package com.wuxi.ads;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
+@MapperScan(value = "com.wuxi.ads.mapper")
 public class AutoDisplayApplication {
 
     public static void main(String[] args) {
